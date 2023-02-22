ada9ba807e319bdae68832b3dc16934864ff411a
Stephen Wall <stephen@kmcmh.com>
2023-02-22 13:40:46 -0700

    changed the tests to be compatible

--- exercises/practice/reverse-string/test/test.dart
+++ exercises/practice/reverse-string/test/test.dart
@@ -1,36 +1,32 @@
-import 'package:reverse_string/reverse_string.dart';
-import 'package:test/test.dart';
-
 void main() {
-  group('ReverseString', () {
-    test('an empty string', () {
-      final result = reverse('');
-      expect(result, equals(''));
-    }, skip: false);
-
-    test('a word', () {
-      final result = reverse('robot');
-      expect(result, equals('tobor'));
-    }, skip: true);
-
-    test('a capitalized word', () {
-      final result = reverse('Ramen');
-      expect(result, equals('nemaR'));
-    }, skip: true);
-
-    test('a sentence with punctuation', () {
-      final result = reverse('I\'m hungry!');
-      expect(result, equals('!yrgnuh m\'I'));
-    }, skip: true);
-
-    test('a palindrome', () {
-      final result = reverse('racecar');
-      expect(result, equals('racecar'));
-    }, skip: true);
-
-    test('an even-sized word', () {
-      final result = reverse('drawer');
-      expect(result, equals('reward'));
-    }, skip: true);
-  });
+    if (reverse('') == '') { 
+      _result(true, ['empty string passed']); 
+    } else { 
+      _result(false, ['empty string fails']); 
+    } 
+    if (reverse('robot') == 'tobor') { 
+      _result(true, ['a word passed']); 
+    } else { 
+      _result(false, ['a word fails']); 
+    } 
+    if (reverse('Ramen') == 'nemaR') { 
+      _result(true, ['a capitalized word passed']); 
+    } else { 
+      _result(false, ['a capitalized word fails']); 
+    } 
+    if (reverse('I\'m hungry!') == '!yrgnuh m\'I') { 
+      _result(true, ['a sentence with punctuation passed']); 
+    } else { 
+      _result(false, ['a sentence with punctuation fails']); 
+    } 
+    if (reverse('drawer') == 'reward') { 
+      _result(true, ['an even-sized word passed']); 
+    } else { 
+      _result(false, ['an even-sized word fails']); 
+    } 
+    if (reverse('racecar') == 'racecar') { 
+      _result(true, ['a palindrome passed']); 
+    } else { 
+      _result(false, ['a palindrome fails']); 
+    } 
 }
