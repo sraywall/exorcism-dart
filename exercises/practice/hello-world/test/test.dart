cdc7a4264815346e4c451df6369215a90570415c
Stephen Wall <stephen@kmcmh.com>
2023-02-22 12:33:36 -0700

    touched up test

--- exercises/practice/hello-world/test/test.dart
+++ exercises/practice/hello-world/test/test.dart
@@ -1,5 +1,5 @@
 void main() {
-    if (HelloWorld().hello().equals('Hello,World!')) { 
+    if (HelloWorld().hello() == 'Hello, World!') { 
       _result(true); 
     } else { 
       _result(false, ['That\'s not quite right. Keep trying!']); 
