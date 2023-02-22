72d052ac42c45e60f14eb1ba2efc14e04031f3e7
Stephen Wall <stephen@kmcmh.com>
2023-02-22 14:00:29 -0700

    altered tests to behave better

--- exercises/practice/reverse-string/test/test.dart
+++ exercises/practice/reverse-string/test/test.dart
@@ -1,32 +1,17 @@
 void main() {
-    if (reverse('') == '') { 
-      _result(true, ['empty string passed']); 
-    } else { 
+    if (reverse('') != '') { 
       _result(false, ['empty string fails']); 
-    } 
-    if (reverse('robot') == 'tobor') { 
-      _result(true, ['a word passed']); 
-    } else { 
+    } else if (reverse('robot') != 'tobor') { 
       _result(false, ['a word fails']); 
-    } 
-    if (reverse('Ramen') == 'nemaR') { 
-      _result(true, ['a capitalized word passed']); 
-    } else { 
+    } else if (reverse('Ramen') != 'nemaR') {
       _result(false, ['a capitalized word fails']); 
-    } 
-    if (reverse('I\'m hungry!') == '!yrgnuh m\'I') { 
-      _result(true, ['a sentence with punctuation passed']); 
-    } else { 
+    } else if (reverse('I\'m hungry!') != '!yrgnuh m\'I') { 
       _result(false, ['a sentence with punctuation fails']); 
-    } 
-    if (reverse('drawer') == 'reward') { 
-      _result(true, ['an even-sized word passed']); 
-    } else { 
+    } else if (reverse('drawer') != 'reward') { 
       _result(false, ['an even-sized word fails']); 
-    } 
-    if (reverse('racecar') == 'racecar') { 
-      _result(true, ['a palindrome passed']); 
-    } else { 
+    } else if (reverse('racecar') != 'racecar') { 
       _result(false, ['a palindrome fails']); 
-    } 
+    } else {
+      _result(true);
+    }   
 }
