7b486c619383839402b938e40756d58d3b51588b
Stephen Wall <stephen@kmcmh.com>
2023-03-15 11:43:31 -0600

    added one line ternary to solution

--- exercises/practice/leap/solution.dart
+++ exercises/practice/leap/solution.dart
@@ -5,6 +5,9 @@ class Leap {
       return year % 400 == 0;
     }
     return year % 4 == 0;
+    // one line ternary preferred
+    // return year % 100 == 0 ? year % 400 == 0 : year % 4 == 0;
   }
+
 }
 
