a7116371dc466da59121979a74a7bc5389e7a7be
Stephen Wall <stephen@kmcmh.com>
2023-03-20 09:18:57 -0600

    added partial solution and tests

--- exercises/practice/armstrong-numbers/solution.dart
+++ exercises/practice/armstrong-numbers/solution.dart
@@ -1,16 +1,8 @@
-score(String s){
-    Map<String, int> letterValues = {
-    "a": 1, "b": 3, "c": 3, "d": 2, "e": 1,
-    "f": 4, "g": 2, "h": 4, "i": 1, "j": 8,
-    "k": 5, "l": 1, "m": 3, "n": 1, "o": 1,
-    "p": 3, "q": 10, "r": 1, "s": 1, "t": 1,
-    "u": 1, "v": 4, "w": 4, "x": 8, "y": 4,
-    "z": 10
-  };
-  
-  //favor one liner functional style
-  return s.toLowerCase().split('').map((ch)=>letterValues[ch]??0).fold(0,(val,sum) => val + sum );
-  
-  //reduce can only be used on non empty collections
-//   return s.toLowerCase().split('').map((ch)=>letterValues[ch]??0).reduce((val,sum) => val + sum );
+//Doesn't work because integers not large enough to hold last tests
+import 'dart:math';
+class ArmstrongNumbers {
+  isArmstrongNumber(String num){ 
+    return num.split('').map((ch)=>pow(int.parse(ch),num.length)).reduce((curr,sum)=>curr+sum).toString() == num;
+  }
 }
+
--- exercises/practice/armstrong-numbers/test/test.dart
+++ exercises/practice/armstrong-numbers/test/test.dart
@@ -1,63 +1,56 @@
-import 'package:armstrong_numbers/armstrong_numbers.dart';
-import 'package:test/test.dart';
 
 void main() {
   final armstrongNumbers = ArmstrongNumbers();
+  
+  List<String> fail_array = [];
 
-  group('ArmstrongNumbers', () {
-    test('Zero is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('0');
-      expect(result, equals(true));
-    }, skip: false);
-
-    test('Single-digit numbers are Armstrong numbers', () {
-      final result = armstrongNumbers.isArmstrongNumber('5');
-      expect(result, equals(true));
-    }, skip: true);
-
-    test('There are no two-digit Armstrong numbers', () {
-      final result = armstrongNumbers.isArmstrongNumber('10');
-      expect(result, equals(false));
-    }, skip: true);
-
-    test('Three-digit number that is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('153');
-      expect(result, equals(true));
-    }, skip: true);
-
-    test('Three-digit number that is not an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('100');
-      expect(result, equals(false));
-    }, skip: true);
-
-    test('Four-digit number that is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('9474');
-      expect(result, equals(true));
-    }, skip: true);
-
-    test('Four-digit number that is not an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('9475');
-      expect(result, equals(false));
-    }, skip: true);
-
-    test('Seven-digit number that is an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('9926315');
-      expect(result, equals(true));
-    }, skip: true);
-
-    test('Seven-digit number that is not an Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('9926314');
-      expect(result, equals(false));
-    }, skip: true);
-
-    test('Armstrong number containing seven zeroes', () {
-      final result = armstrongNumbers.isArmstrongNumber('186709961001538790100634132976990');
-      expect(result, equals(true));
-    }, skip: true);
-
-    test('The largest and last Armstrong number', () {
-      final result = armstrongNumbers.isArmstrongNumber('115132219018763992565095597973971522401');
-      expect(result, equals(true));
-    }, skip: true);
-  });
+  if(armstrongNumbers.isArmstrongNumber('0') != true){
+    fail_array.add('Zero is an Armstrong number');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('5') != true){
+    fail_array.add('Single-digit numbers are Armstrong numbers');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('10') != false){
+    fail_array.add('There are no two-digit Armstrong numbers');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('153') != true){
+    fail_array.add('Three digit number is an Armstrong numbers');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('100') != false){
+    fail_array.add('Three-digit number that is not an Armstrong number');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('9474') != true){
+    fail_array.add('Four-digit number that is an Armstrong number');
+  }  
+  
+  if(armstrongNumbers.isArmstrongNumber('9475') != false){
+    fail_array.add('Four-digit number that is not an Armstrong number');
+  }   
+  
+  if(armstrongNumbers.isArmstrongNumber('9926315') != true){
+    fail_array.add('Seven-digit number that is an Armstrong number');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('9926314') != false){
+    fail_array.add('Seven-digit number that is not an Armstrong number');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('186709961001538790100634132976990') != true){
+    fail_array.add('Armstrong number containing seven zeros');
+  }
+  
+  if(armstrongNumbers.isArmstrongNumber('115132219018763992565095597973971522401') != true){
+    fail_array.add('The largest and last Armstrong number');
+  }
+  if(fail_array.isNotEmpty){
+    _result(false,fail_array);
+  } else {
+    _result(true, ["all sections pass!"]);
+  }
+  
 }
