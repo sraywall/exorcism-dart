1a54fd38d31bef71e05ca1ea551a816359e6bc4f
Stephen Wall <stephen@kmcmh.com>
2023-04-12 09:07:25 -0600

    made solution with BigInt

--- exercises/practice/armstrong-numbers/solution.dart
+++ exercises/practice/armstrong-numbers/solution.dart
@@ -1,8 +1,9 @@
-//Doesn't work because integers not large enough to hold last tests
+
 import 'dart:math';
 class ArmstrongNumbers {
-  isArmstrongNumber(String num){ 
-    return num.split('').map((ch)=>pow(int.parse(ch),num.length)).reduce((curr,sum)=>curr+sum).toString() == num;
+  isArmstrongNumber(String num){
+//     Doesn't work because integers not large enough to hold last tests
+//     return num.split('').map((ch)=>pow(int.parse(ch),num.length)).reduce((curr,sum)=>curr+sum).toString() == num;
+    return num.split('').map((ch)=>BigInt.parse(ch).pow(num.length)).reduce((curr,sum)=>curr+sum).toString() == num;
   }
 }
-
