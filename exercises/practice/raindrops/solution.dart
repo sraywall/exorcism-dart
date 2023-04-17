4ee8ea766ee1414b4441fcfc2864a48e1a0e0887
Stephen Wall <stephen@kmcmh.com>
2023-04-17 12:16:33 -0600

    added files for execution on dartpad

---
+++ exercises/practice/raindrops/solution.dart
@@ -0,0 +1,12 @@
+import 'package:collection/collection.dart';
+
+class WordCount {
+  countWords(String str){
+    var dict = <String,int>{};
+    for(var match in RegExp(r"(\w+('\w+)?)").allMatches(str.toLowerCase())){
+      var s = match[0]??"";
+      dict[s] = (dict[s]??0) + 1;
+    }
+    return dict;
+  }
+}
