bbd5af8ad17dab53ba9b19eebb15c6da228db147
Stephen Wall <stephen@kmcmh.com>
2023-04-17 09:37:59 -0600

    added a working solution

--- exercises/practice/space-age/solution.dart
+++ exercises/practice/space-age/solution.dart
@@ -1,19 +1,22 @@
-class Bob {
-  response(String str){
-    var letters = [for(var match in RegExp(r"[A-Za-z]").allMatches(str)) if(match[0] != null) match[0]??""];
-    bool allCaps = letters.isEmpty ? false : letters.every((s) =>s.toUpperCase() == s);
-    bool question = RegExp(r"\?\s*$").hasMatch(str);
-    bool silence = !RegExp(r"\S").hasMatch(str);
-    if(silence){
-      return 'Fine. Be that way!';
-    }
-    if(question){
-      if(allCaps){
-        return 'Calm down, I know what I\'m doing!';
-      } else {
-        return 'Sure.';
-      }
-    }
-    return allCaps ? 'Whoa, chill out!' : 'Whatever.';
+class SpaceAge {
+  
+    Map<String, num> planets = {
+    'Mercury': 0.2408467,
+    'Venus': 0.61519726,
+    'Earth': 1.0,
+    'Mars': 1.8808158,
+    'Jupiter': 11.862615,
+    'Saturn': 29.447498,
+    'Uranus': 84.016846,
+    'Neptune': 164.79132,
+  };
+  
+  var secondsInDay = 24 * 60 * 60;
+  var daysInYear = 365.25;
+  
+  double age({required planet,required seconds}){
+    var out = (seconds / ((planets[planet] ?? 0.0) * secondsInDay * daysInYear) * 100).round() / 100.0;
+    return out;
   }
 }
+
